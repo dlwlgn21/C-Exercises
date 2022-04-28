b324cccae2ff9d3bb1378d1f9539b98891b8b289
DESKTOP-D4V9PEA\USER <dlwlgn213@gmail.com>
2022-04-28 23:45:27 +0900

    resolve 38

--- C++Exercises/Exercises.cpp
+++ C++Exercises/Exercises.cpp
@@ -84,8 +84,6 @@ void IsPrimeNumer()
 	std::cout << "enter you would like to know is prime number : " << endl;
 	std::cin >> input;
 	assert(input >= 1);
-	int measure = 0;
-	bool isMeetMeasure = false;
 	for (int i = 2; i <= input; ++i)
 	{
 		if (input % i == 0 && i != input)
@@ -778,37 +776,36 @@ void GetNineSumOfTheSeries()
 	InputNumber(term, "Input number of terms: ");
 
 	long sum{};
-	const int TEN = 10;
+	long out = 10;
 	for (int i = 1; i <= term; ++i)
 	{
-		int num{1};
-		for (int j = 1; j <= i; ++j)
+		std::cout << out - 1 << " ";
+		if (i < term)
 		{
-			num *= TEN;
+			std::cout << " + ";
 		}
-		int res = num - 1;
-		sum += res;
-		std::cout << res << " ";
-
+		sum += out - 1;
+		out = (out * 10);
 	}
 	std::cout << std::endl;
 	std::cout << "The sum of the series upto " << term <<" terms: " << sum << std::endl;
-	/* 정답코드
+}
+void GetNineSumOfTheSeriesAnswer()
+{
 	long int n, i, t = 9;
-    int sum = 0;
-    cout << "\n\n Display the sum of the series [ 9 + 99 + 999 + 9999 ...]\n";
-    cout << "-------------------------------------------------------------\n";
-    cout << " Input number of terms: ";
-    cin >> n;
+	int sum = 0;
+	cout << "\n\n Display the sum of the series [ 9 + 99 + 999 + 9999 ...]\n";
+	cout << "-------------------------------------------------------------\n";
+	cout << " Input number of terms: ";
+	cin >> n;
 
-    for (i = 1; i <= n; i++) 
-    {
-        sum += t;
-        cout << t << "  ";
-        t = t * 10 + 9;
-    }
-    cout << "\n The sum of the sarise = " << sum << endl;
-	*/
+	for (i = 1; i <= n; i++)
+	{
+		sum += t;
+		cout << t << "  ";
+		t = t * 10 + 9;
+	}
+	cout << "\n The sum of the sarise = " << sum << endl;
 }
 /*
 24. Write a program in C++ to display the sum of the series [ 1+x+x^2/2!+x^3/3!+.... ].
@@ -950,14 +947,26 @@ void GetSumOfSeriesMysteryThree()
 	int term;
 	InputNumber(term, "Input number of terms: ");
 	int times{ 1 };
-	int sum{1};
+	int sum{0};
+	/*
 	for (int i = 1; i < term; ++i)
 	{
 		std::cout << times << std::endl;
 		times *= 10;
 		sum += GetOneSumRecrisive(times);
 	}
-
+	*/
+	for (int i = 1; i <= term; ++i)
+	{
+		std::cout << times << " ";
+		if (i < term)
+		{
+			std::cout << " + ";
+		}
+		sum += times;
+		times = (times * 10) + 1;
+	}
+	std::cout << std::endl;
 	std::cout << "The sum of the series is :" << sum << std::endl;
 }
 void GetSumOfSeriesMysteryThreeAnswerCode()
@@ -998,6 +1007,7 @@ static int GetFibonacciRecursive(int num)
 	{
 		return 1;
 	}
+
 	return GetFibonacciRecursive(num - 2) + GetFibonacciRecursive(num - 1);
 }
 
@@ -1008,15 +1018,368 @@ void GetFibonacci()
 	assert(term > 0);
 	int res = GetFibonacciRecursive(term - 1);
 
-	int first = 0;
-	int second = 1;
-
+	int before, cur, tmp;
+	before = 0;
+	cur = 1;
+	std::cout << "----------" << std::endl;
+	std::cout << "Here is the Fibonacci series upto to " << term << " terms :" << std::endl;
 	for (int i = 0; i < term; ++i)
 	{
+		tmp = cur;
+		cur = before + cur;
+		std::cout << before << " ";
+		before = tmp;
+	}
+	std::cout << std::endl;
+
+	
+}
+/*
+28. Write a program in C++ to find the number and s
+um of all integer between 100 and 200 which are divisible by 9.
+Sample Output:
+Numbers between 100 and 200, divisible by 9:
+108 117 126 135 144 153 162 171 180 189 198
+The sum : 1683
+*/
+void GetDivisibleByNineBeetween100And200()
+{
+	std::cout << "Numbers between 100 and 200, divisible by 9:" << std::endl;
+	int min, max, sum;
+	sum = 0;
+	min = 9 * 12;
+	max = 200;
+	while (min <= max)
+	{
+		std::cout << min << " ";
+		sum += min;
+		min += 9;
+	}
+
+	std::cout << "The sum : " << sum << std::endl;
+}
+
+/*
+29. Write a program in C++ to find LCM of any two numbers using HCF. Go to the editor
+Sample Output:
+Input 1st number for LCM: 15
+Input 2nd number for LCM: 25
+The LCM of 15 and 25 is: 75
+*/
+
+void GetFirstLCM()
+{
+	int first, second;
+	InputNumber(first, "Input 1st number for LCM: ");
+	InputNumber(second, "Input 2nd number for LCM: ");
+	int min = (first < second) ? first : second;
+	int hcf{1};
+	int lcm{};
+	for (int i = min; i >= 1; --i)
+	{
+		if (first % i == 0 && second % i == 0)
+		{
+			hcf = i;
+			break;
+		}
+	}
+
+	std::cout << first << " and " << second << " LCM is " << (first * second) / hcf << std::endl;
+
+}
+void GetFirstLCMAnswer()
+{
+	int i, n1, n2, j, hcf = 1, lcm;
+	cout << "\n\n LCM of two numbers:\n";
+	cout << "------------------------\n";
+	cout << " Input 1st number for LCM: ";
+	cin >> n1;
+	cout << " Input 2nd number for LCM: ";
+	cin >> n2;
+	j = (n1 < n2) ? n1 : n2;
+	for (i = 1; i <= j; i++) {
+
+		if (n1 % i == 0 && n2 % i == 0) {
+			hcf = i;
+		}
+	}
+	/* mltiplication of HCF and LCM = the multiplication of these two numbers.*/
+	lcm = (n1 * n2) / hcf;
+	cout << " The LCM of " << n1 << " and " << n2 << " is: " << lcm << endl;
+}
+/*
+30. Write a program in C++ to display the number in reverse order. Go to the editor
+Sample Output:
+Input a number: 12345
+The number in reverse order is : 54321
+
+*/
+void GetReverseOrder()
+{
+	int num;
+	InputNumber(num, "Input a number to reverse: ");
+	int cpy = num;
+	int digit {};
+	int cut = {};
+	while (cpy != 0)
+	{
+		cpy /= 10;
+		++digit;
+	}
+	cpy = num;
+	std::cout << num << "'s digit is " << digit << std::endl;
+	for (int i = digit; i >= 1; --i)
+	{
+		int power = 1;
+		for (int j = 0; j < i - 1; ++j)
+		{
+			power *= 10;
+		}
+		cut += (power) * (cpy % 10);
+		std::cout << power << " " << cpy % 10 << std::endl;
+		cpy /= 10;
 
 	}
 
-	std::cout << "Here is the Fibonacci series upto to " << term << " terms :" << res << std::endl;
-	
+
+	std::cout << num << " reverse order is " << cut << std::endl;
+
+}
+void GetReverseOrderAnswer()
+{
+	int num, r, sum = 0, t;
+	cout << "\n\n Display the number in reverse order:\n";
+	cout << "-----------------------------------------\n";
+	cout << " Input a number: ";
+	cin >> num;
+	for (t = num; num != 0; num = num / 10)
+	{
+		r = num % 10;
+		std::cout << "r = num % 10 is  " << r << std::endl;
+
+		sum = sum * 10 + r;
+		std::cout << "sum = sum * 10 + r is " << sum << std::endl;
+	}
+	cout << " The number in reverse order is : " << sum << endl;
 }
 
+/*
+31. Write a program in C++ to find out the sum of an A.P. series. Go to the editor
+Sample Output:
+Input the starting number of the A.P. series: 1
+Input the number of items for the A.P. series: 8
+Input the common difference of A.P. series: 5
+The Sum of the A.P. series are :
+1 + 6 + 11 + 16 + 21 + 26 + 31 + 36 = 148
+*/
+void GetAritimeticProgressionSeries()
+{
+	int first, end, diff;
+	InputNumber(first, "Input the starting number of the A.P. series: ");
+	InputNumber(end, "Input the number of items for the A.P. series: ");
+	InputNumber(diff, "Input the common difference of A.P. series: ");
+	int sum{};
+	int realFirst = first;
+	std::cout << "The Sum of the A.P. series are :" << std::endl;
+	for (int i = 0; i < end; ++i)
+	{
+		sum += first;
+		std::cout << first << " ";
+		if (i < end)
+		{
+			std::cout << " + ";
+		}
+		first += diff;
+		
+	}
+
+	std::cout << " = " << sum << std::endl;
+}
+
+/*
+32. Write a program in C++ to find the Sum of GP series. Go to the editor
+Sample Output:
+Input the starting number of the G.P. series: 3
+Input the number of items for the G.P. series: 5
+Input the common ratio of G.P. series: 2
+The numbers for the G.P. series:
+3 6 12 24 48
+The Sum of the G.P. series: 93
+
+*/
+
+void GetGeomerticProgression()
+{
+	int first, end, ratio;
+	InputNumber(first, "Input the starting number of the A.P. series: ");
+	InputNumber(end, "Input the number of items for the A.P. series: ");
+	InputNumber(ratio, "Input the common difference of A.P. series: ");
+	int sum{};
+	std::cout << "The numbers for the G.P series:" << std::endl;
+	for (int i = 0; i < end; ++i)
+	{
+		sum += first;
+		std::cout << first << " ";
+		first *= ratio;
+	}
+	std::cout << std::endl;
+
+	std::cout << "The numbers for the G.P series:" << sum << std::endl;
+
+}
+/*
+33. Write a program in C++ to Check Whether a Number can be Express as Sum of Two Prime Numbers. Go to the editor
+Sample Output:
+Input a positive integer: 20
+20 = 3 + 17
+20 = 7 + 13
+
+*/
+
+void GetSumTwoPrimeNumber()
+{
+	int num;
+	InputNumber(num, "Input a positive integer: ");
+
+}
+
+
+/*
+34. Write a program in C++ to find the length of a string without using the library function. Go to the editor
+Sample Output:
+Input a string: w3resource.com
+The string contains 14 number of characters.
+So, the length of the string w3resource.com is:14
+*/
+
+void GetStringLength()
+{
+	const size_t BUFFER_LEN = 512;
+	char buffer[BUFFER_LEN];
+	std::cout << "input your string : ";
+	std::cin >> buffer;
+	char* p = buffer;
+	size_t len{};
+	while (*p != '\0')
+	{
+		++p;
+		++len;
+	}
+	std::cout << std::endl;
+	std::cout << "The string contains " << len << " number of characters." << std::endl;
+	std::cout << "So, the length of the string  [" << buffer << "] is " << len << std::endl;
+}
+
+/*
+35. Write a program in C++ to display the pattern like right angle triangle using an asterisk.
+Sample Output:
+Input number of rows: 5
+*
+**
+***
+****
+*****
+
+*/
+
+void PrintTriangle()
+{
+	int rows;
+	InputNumber(rows, "Input number of rows: ");
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+}
+/*
+36. Write a program in C++ to display the pattern like right angle triangle with number. Go to the editor
+Sample Output:
+Input number of rows: 5
+1
+12
+123
+1234
+12345
+*/
+
+void PrintTriangleTwo()
+{
+	int rows;
+	InputNumber(rows, "Input number of rows: ");
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			std::cout << j + 1;
+		}
+		std::cout << std::endl;
+	}
+}
+/*
+37. Write a program in C++ to make such a pattern like right angle triangle 
+using number which will repeat the number for that row.
+Sample Output:
+  Input number of rows: 5
+1
+22
+333
+4444
+55555
+
+*/
+void PrintTriangleThree()
+{
+	int rows;
+	InputNumber(rows, "Input number of rows: ");
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			std::cout << i + 1;
+		}
+		std::cout << std::endl;
+	}
+}
+
+/*
+38. Write a program in C++ to make such a pattern like right angle triangle with number increased by 1. 
+Sample Output:
+ Input number of rows: 4
+1
+2 3
+4 5 6
+7 8 9 10
+
+*/
+
+void PrintTriangleFour()
+{
+	int rows;
+	InputNumber(rows, "Input number of rows: ");
+	int count{};
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			std::cout << ++count;
+		}
+		std::cout << std::endl;
+	}
+}
+
+/*
+39. Write a program in C++ to make such a pattern like a pyramid with numbers increased by 1.
+Sample Output:
+ Input number of rows: 4
+	   1
+	  2 3
+	 4 5 6
+	7 8 9 10
+*/
--- C++Exercises/main.cpp
+++ C++Exercises/main.cpp
@@ -1,38 +1,84 @@
 #include <iostream>
 #include "Exercises.h"
 
-#define NOT_FOUND (-1)
+
+static bool BinarySearchRecursiveTwo(unsigned int arry[], int lIdx, int rIdx, int value)
+{
+	if (lIdx > rIdx)
+	{
+		return false;
+	}
+
+	int mid = (lIdx + rIdx) / 2;
+	if (arry[mid] == value)
+	{
+		return true;
+	}
+	else if (arry[mid] < value)
+	{
+		BinarySearchRecursiveTwo(arry, mid + 1, rIdx, value);
+	}
+	else
+	{
+		BinarySearchRecursiveTwo(arry, lIdx, mid - 1, value);
+	}
+
+
+}
+
+static void BubbleSort(unsigned int arry[], size_t size)
+{
+	for (int i = 0; i < size - 1; ++i)
+	{
+		for (int j = 0; j < size - i - 1; ++j)
+		{
+			if (arry[j] > arry[j + 1])
+			{
+				unsigned int tmp = arry[j];
+				arry[j] = arry[j + 1];
+				arry[j + 1] = tmp;
+			}
+		}
+	}
+
+}
 
 int main()
 {
+#if 0
+	const int SIZE = 16;
+	unsigned int arry[SIZE] =
+	{
+		2, 72, 1, 32, 27, 35, 11, 25,
+		12, 15, 85, 22, 98, 9, 24, 7
+	};
 
-	//GetPerfectNumberBeetween1And500();
-	//IsPrimeNumer();
-	//GetPrimenumberRange();
-	//GetFactorial();
-	// 재귀함수로 올바르게 짰는지 궁금합니다. 
-	//GetLastPrimeNumber();
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << arry[i] << " ";
+	}
+	std::cout << std::endl;
 
-	//GetGreatestCommonDivisorOfTwoNumbers();
-	//GetSumOfDigit();
-	//GetSumOfTheSerise();
-	//GetCalculateTheSumOfTheSeries();
-	//GetCalculateTheSeries();
-	//GetFindTheSumOfSeriesUPToNthTerm();
-	//GetMaxMinAverage();
-	//GetNonPrimeNumbers();
-	//PrintSquare();
-	//PrintTheCubeOfNumber();
-	//PrintMultiplicationVerticaly();
-	//GetOddSum();
-	//GetEvenSum();
-	//GetHarmonicSeriesSum();
-	//GetNineSumOfTheSeries();
-	//GetSumOfSeriesMystery();
-	//GetSumOfSeriesMysteryAnswerCode();
-	//GetSumOfSeriesMysteryTwo();
-	//GetSumOfSeriesMysteryTwoAnswerCode();
-	//GetSumOfSeriesMysteryThreeAnswerCode();
-	//GetSumOfSeriesMysteryThree();
-	GetFibonacci();
+	BubbleSort(arry, SIZE);
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << arry[i] << " ";
+	}
+	std::cout << std::endl;
+	int searchNum = 99;
+	bool result = BinarySearchRecursiveTwo(arry, 0, SIZE - 1, searchNum);
+
+	std::cout << "value " << searchNum << " is In arry";
+	if (result)
+	{
+		std::cout << " Has Value!! Has " << searchNum << std::endl;
+	}
+	else
+	{
+		std::cout << " NOT Has Value. there is NO " << searchNum << std::endl;
+
+	}
+#endif
+	PrintTriangleFour();
 }
